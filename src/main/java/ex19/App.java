38c80dd8129ccba4f375b3dcfe1468e29f130d86
savosburn <savosburn18@tampabay.rr.com>
2021-06-04 23:45:37 -0400

    bedtime :)

--- src/main/java/ex19/App.java
+++ src/main/java/ex19/App.java
@@ -10,12 +10,14 @@ import java.util.Scanner;
 /*
 You’ll often need to see if one value is within a certain range and alter the flow of a program as a result.
 
-Create a program to calculate the body mass index (BMI) for a person using the person’s height in inches and weight in pounds. The program should prompt the user for weight and height.
+Create a program to calculate the body mass index (BMI) for a person using the person’s height in inches and weight in
+pounds. The program should prompt the user for weight and height.
 
 Calculate the BMI by using the following formula:
 
 bmi = (weight / (height × height)) * 703
-If the BMI is between 18.5 and 25, display that the person is at a normal weight. If they are out of that range, tell them if they are underweight or overweight and tell them to consult their doctor.
+If the BMI is between 18.5 and 25, display that the person is at a normal weight.
+If they are out of that range, tell them if they are underweight or overweight and tell them to consult their doctor.
 
 Example Output
 Your BMI is 19.5.
@@ -33,6 +35,73 @@ public class App {
 
     public static void main(String[] args) {
         App myApp = new App();
+
+        double height = myApp.readHeight();
+        double weight = myApp.readWeight();
+        double bmi = myApp.calculateBMI(height, weight);
+
+        String outputString = myApp.generateString(bmi);
+        myApp.output(outputString);
     }
 
+    public double readHeight() {
+        //double number;
+
+        /*
+        while(!in.hasNextDouble()) {
+            System.out.print("Please enter your height in inches: ");
+
+            if () {
+                System.out.printf("Invalid number. Please try again.\n");
+                scan.next();
+            }
+
+            number = in.nextDouble();
+            return number;
+        }*/
+
+        System.out.print("Please enter your height in inches: ");
+        while(!in.hasNextInt()) {
+            System.out.printf("Invalid number. Please try again.\n");
+            in.next();
+        }
+        double number = in.nextDouble();
+
+        return number;
+    }
+
+    public double readWeight() {
+
+        System.out.print("Please enter your weight in pounds: ");
+        while(!in.hasNextInt()) {
+            System.out.printf("Invalid number. Please try again.\n");
+            in.next();
+        }
+        double number = in.nextDouble();
+
+        return number;
+    }
+
+    public double calculateBMI(double height, double weight) {
+        return (weight / (height * height)) * 703;
+    }
+
+    public String generateString(double bmi) {
+        String str1 = String.format("Your BMI is %.1f", bmi);
+        String str2 = null;
+
+        if (bmi < 18.5) {
+            str2 = "You are underweight. You should see your doctor.";
+        } else if (bmi < 25) {
+            str2 = "Your weight is normal.";
+        } else {
+            str2 = "You are overweight. You should see your doctor.";
+        }
+
+        return str1 + str2;
+    }
+
+    public void output(String outputString) {
+        System.out.println(outputString);
+    }
 }
